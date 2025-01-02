97cdc001f4c99c169d0d1acd24c3582ec11409c4
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2025-01-02 18:13:50 +0530

    Remove old index.html, index.js, and style.css; update test folder files with new structure and styles

--- Projects/index.js
+++
@@ -1,8 +0,0 @@
-// const link = document.createElement('a');
-
-// link.href = 'https://images.unsplash.com/photo-1500113018993-b5c64275ed93?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-
-// link.download = 'file.pdf'; // Suggest a filename
-// document.body.a.appendChild(link);
-// link.click();
-// document.body.removeChild(link);
