7388775403eb6eff4cdecf660e9140a15601e303
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2025-01-16 17:52:46 +0530

    Add new variable calculations and comment out console logs for cleaner output

--- test folder/Javascript/Odin Variable & Operator.js
+++ test folder/Javascript/Odin Variable & Operator.js
@@ -1,8 +1,12 @@
 const pi = 3.14;
 // pi = 1;
 
-console.log(pi);
+// console.log(pi);
 
 let a = 1;
 a = a + 5;
-console.log(a);
+// console.log(a);
+
+let val1 = 5;
+let b = val1 * 10;
+console.log(b);
