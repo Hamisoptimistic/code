3ae1ceefa93a54ee543ba1576a5b860ef1ffdc84
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2025-01-18 16:43:39 +0530

    Refactor sumDouble function for clarity and add test cases for validation

--- Coding_JS problems/sumDouble.js
+++ Coding_JS problems/sumDouble.js
@@ -1,3 +1,9 @@
-function sumDouble(a, b){
-  return a===b? 2*(a+b): a+b
+function sumDouble(a, b) {
+  return a === b ? 2 * (a + b) : a + b;
 }
+
+sumDouble(1, 2); // 3
+console.log(sumDouble(3, 2)); // 5
+console.log(sumDouble(2, 2)); // 8
+console.log(sumDouble(3, 3)); // 12
+console.log(sumDouble(0, 0)); // 0
