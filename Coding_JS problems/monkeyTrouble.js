09ef4258e70cff5591117b2234eefcd90dfbea79
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2025-01-18 16:43:05 +0530

    Implement monkeyTrouble function and add sumDouble function; include test cases for monkeyTrouble

--- Coding_JS problems/monkeyTrouble.js
+++ Coding_JS problems/monkeyTrouble.js
@@ -1,4 +1,8 @@
-function monkeyTrouble(aSmile, bSmile){
-  return (aSmile && bSmile) || !aSmile && !bSmile
-  
+function monkeyTrouble(aSmile, bSmile) {
+  return (aSmile && bSmile) || (!aSmile && !bSmile);
 }
+
+console.log(monkeyTrouble(true, true)); // true
+monkeyTrouble(false, false); // true
+monkeyTrouble(true, false); // false
+monkeyTrouble(false, true); // false
