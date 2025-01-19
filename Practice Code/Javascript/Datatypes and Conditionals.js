62fd5bb53c3fa41b4bf18765966e1b65d2c47142
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2025-01-19 22:53:29 +0530

    Add string manipulation examples and new file for datatype comparisons

--- Practice Code/Javascript/Datatypes and Conditionals.js
+++ Practice Code/Javascript/Datatypes and Conditionals.js
@@ -1,1 +1,11 @@
-const string = 
+const string = 'She said "I\'m not going in there!"';
+
+// console.log(string);
+console.log(string.length);
+console.log(string);
+console.log(string.charAt(2));
+console.log(string.at(-1));
+
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(text);
---
+++ Practice Code/Javascript/Odin Assignment/Datatypes and comparisons.js
@@ -0,0 +1,37 @@
+// // 1ST TASK
+
+// /**
+//  * ===== Troubleshooting =====
+//  * The function below should log the number 2, however it does not,
+//  * see if you can fix it!
+//  * Be sure to fix it in the spirit of the code, do not hard code the result.
+//  */
+
+// function troubleshooting() {
+//   const a = 1;
+//   const b = 1;
+
+//   let result;
+
+//   // Edit between these lines
+//   // =================================
+//   result = a + b;
+
+//   // =================================
+
+//   return result;
+// }
+
+// console.log(troubleshooting()); // Expected output: 2
+
+// 2nd TASK
+
+function numberChecker(number) {
+  if (number === 6) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(numberChecker(6));
