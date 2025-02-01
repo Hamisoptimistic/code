ca4fd595f480c64825c0052223be405802cfae33
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2025-02-01 15:44:55 +0530

    Add hasTeen function for age checking and update workspace with recent files

--- Practice Code/Javascript/Odin Assignment/Datatypes and comparisons.js
+++ Practice Code/Javascript/Odin Assignment/Datatypes and comparisons.js
@@ -26,12 +26,37 @@
 
 // 2nd TASK
 
-function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function numberChecker(number) {
+//   if (number === 6) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-console.log(numberChecker(6));
+// console.log(numberChecker(6));
+
+/**
+ * Lets do some math!
+ * Some rules first:
+ *   - Replace the strings to the right of the = with the math expression they describe.
+ *   - Do not manually enter the answers to the equations. For example, `const a = 9` would be incorrect as 9 is the answer to the equation you're being asked to write out
+ */
+
+// const a = 1 + 8;
+// const b = 22 * 3;
+// const c = 5 % 4;
+// const d = a - 17;
+// const e = a + b + c + d;
+// console.log(e);
+
+const birthYear = 2000;
+const thisYear = 2025;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const fullName = firstName + lastName;
+const age = thisYear - birthYear;
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+
+console.log(greeting);
