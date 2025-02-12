7c54aa178ee99ec1c2adf76d4cf9774d7218af98
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2025-02-12 14:51:23 +0530

    Add computer and human move functions to Rock Paper Scissors game

--- Rock paper Scissor/script.js
+++ Rock paper Scissor/script.js
@@ -1,1 +1,35 @@
-console.log("this shit right here");
+// Rock Paper scissor
+// Generate random move for computer
+//
+
+let getComputermove = () => {
+  let number = Math.random();
+  let randomNum = number.toFixed(1);
+  // console.log(randomNum);
+
+  // Computer Logic
+  let computerMove = "";
+  if (randomNum <= 0.3) {
+    computerMove = "rock";
+  } else if (randomNum > 0.3 && randomNum <= 0.7) {
+    computerMove = "paper";
+  } else {
+    computerMove = "scissor";
+  }
+  // console.log(computerMove);
+  return computerMove;
+};
+let computerChoice = getComputermove();
+
+let getHumanMove = () => {
+  let input = prompt("What's Your Choice??");
+  let humanChoice = input.toLowerCase().trim();
+  // console.log(playerChoice);
+  return humanChoice;
+};
+
+let playerChoice = getHumanMove();
+
+let playRound = (a, b) => {};
+
+playRound(computerChoice, playerChoice);
