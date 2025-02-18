a699a1ba6d9b79c494517dc648a980c0d748646b
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2025-02-18 14:07:15 +0530

    Add Js.info tasks.js file and update workspace with new file path

---
+++ Practice Code/Javascript/Js.info tasks.js
@@ -0,0 +1,9 @@
+let styles = ["jazz", "blues"];
+let length = styles.push("Rock and Roll");
+let middlePos = Math.floor(length / 2);
+styles[(middlePos = "Classics")];
+const fristVal = styles.shift();
+console.log(styles);
+console.log(fristVal);
+styles.unshift("Rap", "Reggae");
+console.log(styles);
